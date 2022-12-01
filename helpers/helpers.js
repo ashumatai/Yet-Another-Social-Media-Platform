31afb0f3c2d12f4aae0c51a05f98eef60fcccec6
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2022-12-01 11:59:00 -0500

    Added 3 more validation functions

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -41,11 +41,44 @@ const validStatus = function error_handling_for_status(inputStatus,inputParamete
     if(!statusFormat.includes(inputStatus.trim())) throw 'Please enter valid '+ inputParameter+' type';
     
 }
+const validLogin = function error_handling_for_login(inputUsername,inputPassword){
+  let format = /[` !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  let spaces = /(.*\s{1,}.*)|(^\s+.*)|(.*\s+$)/g;
+  let password_format = /^(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/g;
+  inputUsername = inputUsername.trim();
+  if (!inputUsername || !inputPassword) throw 'You must provide username and password ';
+  if (typeof(inputUsername) !== 'string' || typeof(inputUsername)==='undefined') throw 'Username must be a string';
+  if (typeof(inputPassword) !== 'string' || typeof(inputPassword)==='undefined') throw 'Password must be a string';
+  if (inputUsername.length < 4) throw 'Username should be at least 4 characters long.';
+  // if(spaces.test(inputUsername)) throw 'Username cannot contain spaces';
+  if(format.test(inputUsername)) throw 'Please enter valid username .i.e without special characters or spaces';
+  if (inputPassword.length < 6) throw 'Password should be at least 6 characters long.';
+  if(spaces.test(inputPassword)) throw 'Password cannot contain spaces';
+  if(!password_format.test(inputPassword)) throw 'Password should contain at least one uppercase character, at least one number and at least one special character';
+
+}
+const validEmail = function error_handling_for_email(inputEmail){
+  let emailFormat = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/g;
+  inputEmail = inputEmail.trim();
+  if (!inputEmail) throw 'You must provide Email Address ';
+  if (typeof(inputEmail) !== 'string' || typeof(inputEmail)==='undefined') throw 'Email Address must be a string';
+  if(!emailFormat.test(inputEmail)) throw 'Please enter a valid Email Address';
+}
+const validPhoneNumber = function error_handling_for_phoneNumber(inputPhoneNumber){
+  let mobileFormat = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s-]\d{3}[\s-]\d{4}$/g;
+  inputPhoneNumber = inputPhoneNumber.trim();
+  if (!inputPhoneNumber) throw 'You must provide Phone Number ';
+  if (typeof(inputPhoneNumber) !== 'string' || typeof(inputPhoneNumber)==='undefined') throw 'Phone Number must be a string';
+  if(!mobileFormat.test(inputPhoneNumber)) throw 'Please enter a valid Phone Number';
+}
 module.exports = {
    
     validObjectId:validObjectId,
     validName:validName,
     validStatus:validStatus,
-    validString:validString
+    validString:validString,
+    validLogin:validLogin,
+    validEmail:validEmail,
+    validPhoneNumber:validPhoneNumber
    
   };
