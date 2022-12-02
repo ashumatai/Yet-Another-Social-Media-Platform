980e7c7b778883c7365c6ce93b7ef83b33061bb5
deepali04 <nagwade.deepali@gmail.com>
2022-12-02 01:17:43 -0500

    Added routes definition in users and posts routes

---
+++ routes/posts.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const movieData = data.movies;
+const {ObjectId} = require('mongodb');
+
+
+router
+  .route('/').get(async (req, res) => {
+    
+  });
+
+router
+  .route('/:postId').get(async (req, res) => {
+   
+});
+
+router
+  .route('/:postId').delete(async (req, res) => {
+     
+  });
+  
+  router
+  .route('/:postId').put(async (req, res) => {
+
+});
+  module.exports = router;
