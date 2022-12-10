a1d6e46bab5d08abdc064987fd6830b394f050f7
deepali04 <nagwade.deepali@gmail.com>
2022-12-10 00:17:13 -0500

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./users');
-const postsRoutes= require('./posts')
+const postsRoutes= require('./posts');
+const savedPosts= require('./savedPosts');
 
 const constructorMethod = (app) => {
   app.use('/', userRoutes);
   app.use('/', postsRoutes);
+  app.use('/', savedPosts);
   app.use('*', (req, res) => {
     res.status(404).send("Page not found");
   });
