4731dcae504481b937de95b2dd733fcbcd2273e5
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2022-12-11 14:29:09 -0500

    Added savedPost Routes & modified index.js Routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const userRoutes = require('./users');
 const postsRoutes= require('./posts');
-const savedPosts= require('./savedPosts');
-
+const savedPostsRoutes= require('./savedPosts');
+const homeRoutes= require('./home');
 const constructorMethod = (app) => {
-  app.use('/', userRoutes);
-  app.use('/', postsRoutes);
-  app.use('/', savedPosts);
+  app.use('/users', userRoutes);
+  app.use('/posts', postsRoutes);
+  app.use('/savedPosts', savedPostsRoutes);
+  app.use('/home', homeRoutes);
   app.use('*', (req, res) => {
     res.status(404).send("Page not found");
   });
--- routes/savedPosts.js
+++ routes/savedPosts.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data');
-const movieData = data.movies;
+const userData = require('../data/users');
+const savedPostData = require('../data/savedPosts');
 const {ObjectId} = require('mongodb');
+const validatiion = require('../helpers/validations');
+router
+  .route('/:userId')
+  .get(async (req, res) => {
+    try {
+    
+      if(validatiion.validObjectId(req.params.userId,"ID"));
+      req.params.userId = req.params.userId.trim();
+      const savedPost = await savedPostData.getsavedPost(req.params.userId);
+      res.json(savedPost);
+    } 
+    catch (e) {
+      if(typeof(e)==='object'){
+          res.status(404).send(e);       
+      }
+      else{
+        res.status(400).send(e);
+      }
+    } 
+  })
+  module.exports = router;
