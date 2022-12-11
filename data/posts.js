fbd3f5fb097d947f18965ec9c7a313185b4b950c
deepali04 <nagwade.deepali@gmail.com>
2022-12-11 13:08:19 -0500

    Added data function for getPosts, getPostbyId and deletePost

--- data/posts.js
+++ data/posts.js
@@ -1,27 +1,41 @@
 const mongoCollections = require('../config/mongoCollections');
 const posts = mongoCollections.posts;
+const users = mongoCollections.users;
 const {ObjectId} = require('mongodb');
 const helper = require('../helpers/validations');
-const { posts } = require('./posts');
 
-const getAllPosts = async () => {
+//get all posts of a user
+const getAllPosts = async (userId) => {
+  if (!userId)
+    throw [400,"You must provide an ID"];
+  
+  if (typeof userId !== 'string' || userId.trim().length===0)
+    throw [400,"Please provide a valid ID"];
+  
+  if(!ObjectId.isValid(userId))
+    throw [400,"The ID is not a valid Object ID"];
 
-  const postsCollection= await posts();
-  const postsList = await postsCollection.find().toArray();
-  if (!postsList) 
-    throw 'Could not get all posts';
-  resultArray=[];
-  postsList.forEach(post => {
-    resultObject={}
-    post['_id'] = post['_id'].toString();
-    resultObject._id=post['_id']
-    // resultObject.title=post.title;
-    resultArray.push(resultObject);
-  }); 
+  const userCollection = await users();
+  const user = await userCollection.findOne({ _id: ObjectId(userId)});
 
-  return resultArray;
+  if (user === null)
+      throw [404,"No User found with that ID"];
+
+  if(user.userPosts.length===0){
+    throw [404,"No Posts found for this user"];
+  }
+
+  const postCollection = await posts();
+  let postData=[];
+  console.log(user.userPosts);
+  for (const post of user.userPosts) {
+    postData.push(await postCollection.findOne({_id: ObjectId(post)}));
+  }
+  console.log(postData);
+  return postData;
 };
 
+//get post by postId
 const getPostById = async (postId) => {
   if (!postId)
     throw [400,"You must provide an ID"];
@@ -39,6 +53,31 @@ const getPostById = async (postId) => {
   return post;
 };
 
+
+const createPost = async ( postContent, caption, tags, email, comments, likes) => {
+
+  const postCollection = await posts();
+  let newPost = {
+    postContent: postContent,
+    caption: caption,
+    tags: tags,
+    email: email,
+    comments: comments,
+    likes:likes
+  }
+
+  const insertInfo = await postCollection.insertOne(newPost);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId)
+  throw [500,"Could not add new movie"];
+
+  const newId = insertInfo.insertedId.toString();
+  const post = await getPostById(newId.toString());
+  return post;
+};
+
+
+
+//delete a post by postId
 const deletePost = async (postId) => {
   if (!postId)
     throw [400,"provide an ID to search for"];
@@ -63,4 +102,11 @@ if(deletionInfo.deletedCount === 0) {
 }
 resultData = {"postid": postId, "deleted": true};
 return resultData;
+};
+
+module.exports = {
+  getAllPosts,
+  getPostById,
+  deletePost  ,
+  createPost
 };
