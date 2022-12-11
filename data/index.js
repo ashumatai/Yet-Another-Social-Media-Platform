97aa461dfec6274f6542ef37c78d4dfc061a5fe9
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2022-12-11 12:56:07 -0500

    Added savedPost,modified app.js,modified index.js

--- data/index.js
+++ data/index.js
@@ -1,3 +1,12 @@
 conversations = require("./conversations");
 posts = require("./posts");
 users = require("./users");
+savedPost = require("./savedPosts");
+home = require("./home");
+module.exports = {
+    posts:posts,
+    users:users,
+    savedPost:savedPost,
+    conversations:conversations,
+    home:home
+};
---
+++ data/savedPosts.js
@@ -0,0 +1,27 @@
+const validatiion = require('../helpers/validations');
+const mongoCollections = require('../config/mongoCollections');
+const users = mongoCollections.users;
+const posts = mongoCollections.posts;
+const {ObjectId} = require('mongodb');
+
+
+
+const getsavedPost = async (userId) => {
+    if(validatiion.validObjectId(userId,"ID"));
+    userId = userId.trim();
+    const userscollection = await users();
+    const postscollection = await posts();
+    let postData = [];
+    const user_data = await userscollection.findOne({_id: ObjectId(userId)});
+    if(user_data === null) throw {error:'No user found with '+userId};
+    const savedPostData = user_data.savedPosts;
+    for (const data of savedPostData) {
+      postData.push(await postscollection.findOne({_id: ObjectId(data)}));
+    }
+    if(postData === null) throw {error:'No saved posts found  '+userId};
+    return postData; 
+  };
+
+  module.exports = {
+    getsavedPost:getsavedPost,
+  };
