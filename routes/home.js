25c2eef76426832b0adbfeced76e7098fe1edc04
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2022-12-13 13:37:37 -0500

    Modified home.js routes

--- routes/home.js
+++ routes/home.js
@@ -10,8 +10,10 @@ router
   .route('/')
   .get(async (req, res) => {
     try {
-      const posts = await homeData.getAllPost();
-      res.json(posts);
+      // take user id 
+      const posts = await homeData.getAllPost("63963928ac02e3a9db204155");
+      // res.json(posts);
+      return res.render('home/homePage',{title:"Home",postData:posts});
     } 
     catch (e) {
       if(typeof(e)==='object'){
@@ -22,34 +24,35 @@ router
       }
     } 
   });
-  router
-  .route('/followersPost/:userId')
-  .get(async (req, res) => {
-    try {
+  // router
+  // .route('/followersPost/:userId')
+  // .get(async (req, res) => {
+  //   try {
     
-      if(validatiion.validObjectId(req.params.userId,"ID"));
-      req.params.userId = req.params.userId.trim();
-      const followersPost = await homeData.getFollowersPost(req.params.userId);
-      res.json(followersPost);
-    } 
-    catch (e) {
-      if(typeof(e)==='object'){
-          res.status(404).send(e);       
-      }
-      else{
-        res.status(400).send(e);
-      }
-    } 
-  });
+  //     if(validatiion.validObjectId(req.params.userId,"ID"));
+  //     req.params.userId = req.params.userId.trim();
+  //     const followersPost = await homeData.getFollowersPost(req.params.userId);
+  //     res.json(followersPost);
+  //   } 
+  //   catch (e) {
+  //     if(typeof(e)==='object'){
+  //         res.status(404).send(e);       
+  //     }
+  //     else{
+  //       res.status(400).send(e);
+  //     }
+  //   } 
+  // });
   router
-  .route('/followingPost/:userId')
+  .route('/feed/:userId')
   .get(async (req, res) => {
     try {
     
       if(validatiion.validObjectId(req.params.userId,"ID"));
       req.params.userId = req.params.userId.trim();
       const followingPost = await homeData.getFollowingPost(req.params.userId);
-      res.json(followingPost);
+      // res.json(followingPost);
+      return res.render('home/feedPage',{title:"Feed",postData:followingPost});
     } 
     catch (e) {
       if(typeof(e)==='object'){
