b1d4c09e7a8b7144d280f9f1a38ed53e7bb31a2e
Gundeep Singh Saluja <gssaluja77@gmail.com>
2022-12-13 14:22:06 -0500

    Prevented injection attacks with xss on req params

--- data/users.js
+++ data/users.js
@@ -1,19 +1,28 @@
 const { ObjectId } = require("mongodb");
 const { users } = require("../config/mongoCollections");
+const {
+  validObjectId,
+  validString,
+  validAge,
+} = require("../helpers/validations");
 
 const getAllUsers = async () => {
   const userCollection = await users();
   const userList = await userCollection.find({}).toArray();
   if (!userList)
-    throw { error: "Could not get all the users in the db!", code: 404 };
+    throw { message: "Could not get all the users in the db!", code: 404 };
   return userList;
 };
 
 const getUserById = async (userId) => {
+  validObjectId(userId);
   const userCollection = await users();
   const userObject = await userCollection.findOne({ _id: ObjectId(userId) });
   if (userObject === null)
-    throw { error: "No movie with this ID can be found in the db!", code: 404 };
+    throw {
+      message: "No user with this ID can be found in the db!",
+      code: 404,
+    };
   return userObject;
 };
 
@@ -23,14 +32,84 @@ const deleteAllUsers = async () => {
 };
 
 const deleteUserById = async (userId) => {
+  validObjectId(userId);
   const userCollection = await users();
   const deletionInfo = userCollection.deleteOne({ _id: ObjectId(userId) });
   if (deletionInfo.deletedCount === 0)
     throw {
-      error: `Could not delete user with the user id ${userId}`,
+      message: `Could not delete user with the user id ${userId}`,
       code: 404,
     };
   return { userId: userId, deleted: true };
+};
+
+const updateUserById = async (
+  userId,
+  userName,
+  firstName,
+  lastName,
+  email,
+  phoneNumber,
+  address,
+  city,
+  state,
+  dateOfBirth,
+  age
+) => {
+  if (
+    !userName ||
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phoneNumber ||
+    !address ||
+    !city ||
+    !state ||
+    !dateOfBirth ||
+    !age
+  )
+    throw { message: "All fields must be supplied!", code: 400 };
+
+  validObjectId(userId);
+  validString(userName);
+  validString(firstName);
+  validString(lastName);
+  validString(email);
+  validString(phoneNumber);
+  validString(address);
+  validString(city);
+  validString(state);
+  validString(dateOfBirth);
+  validAge(age);
+
+  const userExists = await getUserById(userId);
+  if (!userExists) throw { message: "User doesn't exist!", code: 400 };
+  const userCollection = await users();
+  const updatedUser = {
+    userName: userName,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phoneNumber: phoneNumber,
+    address: address,
+    city: city,
+    state: state,
+    dateOfBirth: dateOfBirth,
+    age: age,
+  };
+  const updatedInfo = userCollection.updateOne(
+    { _id: ObjectId(userId) },
+    { $set: updatedUser }
+  );
+  if (updatedInfo.modifiedCount === 0)
+    throw {
+      message: `Could not update user with user ID ${userId}!`,
+      code: 404,
+    };
+  else {
+    const newUser = await getUserById(userId);
+    return newUser;
+  }
 };
 
 module.exports = {
@@ -38,4 +117,5 @@ module.exports = {
   getUserById,
   deleteAllUsers,
   deleteUserById,
+  updateUserById,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,114 @@
-const express = require('express');
-const { getAllUsers } = require('../data/users');
+const express = require("express");
+const { users } = require("../config/mongoCollections");
+const { getAllUsers, getUserById, deleteUserById, updateUserById } = require("../data/users");
+const {
+  validObjectId,
+  validString,
+  validAge,
+  validEmail,
+  validPhoneNumber,
+} = require("../helpers/validations");
 const router = express.Router();
+const xss = require("xss");
 
-router
-  .route('/').get(async (req, res) => {
-    try {
-      const allUsers = await getAllUsers();
-      res.json(allUsers);
-    }
-    catch(error) {
-      res.status(error.code).send(error.error);
-    }
-  });
-
-router
-  .route('/:userId').get(async (req, res) => {
-   res.send("Get by id working!")
+router.route("/").get(async (req, res) => {
+  try {
+    const allUsers = await getAllUsers();
+    res.json(allUsers);
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
 });
 
-router
-  .route('/:userId').delete(async (req, res) => {
-     res.send("Delete working!")
-  });
-  
-  router
-  .route('/:userId').put(async (req, res) => {
-    res.send("Put working!")
+router.route("/:userId").get(async (req, res) => {
+  try {
+    validObjectId(req.params.userId);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+  try {
+    const userById = await getUserById(req.params.userId);
+    res.json(userById);
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
 });
-  module.exports = router;
+
+router.route("/:userId").delete(async (req, res) => {
+  try {
+    validObjectId(req.params.userId);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+  try {
+    await getUserById(req.params.userId);
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+  try {
+    const userById = await deleteUserById(req.params.userId);
+    res.json(userById);
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+});
+
+router.route("/:userId").put(async (req, res) => {
+  try {
+    validObjectId(req.params.userId);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+  // checking if the user exists or not in the first place
+  try {
+    await getUserById(req.params.userId);
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+
+  let user = req.body;
+  try {
+    if (
+      !user.userName ||
+      !user.firstName ||
+      !user.lastName ||
+      !user.email ||
+      !user.phoneNumber ||
+      !user.address ||
+      !user.city ||
+      !user.state ||
+      !user.age ||
+      !user.dateOfBirth
+    )
+      throw { message: "All fields must be supplied!", code: 400 };
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+
+  try {
+    validString(user.userName, "Username");
+    validString(user.firstName, "First Name");
+    validString(user.lastName, "Last Name");
+    validString(user.address, "Address");
+    validString(user.city, "City");
+    validString(user.state, "State");
+    validEmail(user.email, "Email");
+    validPhoneNumber(user.phoneNumber, "Phone Number");
+    validString(user.dateOfBirth, "DOB");
+    validAge(user.age, "Age");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+  try {
+    const updatedUser = await updateUserById(req.params.id, xss(user.userName), xss(user.firstName), xss(user.lastName), xss(user.address), xss(user.city), xss(user.state), xss(user.email), xss(user.phoneNumber), xss(user.dateOfBirth), xss(user.age));
+    res.json(updatedUser);
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;
