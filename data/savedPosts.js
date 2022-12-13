b7a79f1d121400ab05876152a7e7234cb6c7d04e
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2022-12-12 20:24:07 -0500

    Added two new data functions to savedpPosts.js

--- data/savedPosts.js
+++ data/savedPosts.js
@@ -22,6 +22,34 @@ const getsavedPost = async (userId) => {
     return postData; 
   };
 
+  const addsavedPost = async (userId,postId) => {
+    if(validatiion.validObjectId(userId,"ID"));
+    if(validatiion.validObjectId(postId,"ID"));
+    userId = userId.trim();
+    postId = postId.trim();
+    const userscollection = await users();
+    const user_data = await userscollection.updateOne({_id: ObjectId(userId)},{$push:{"savedPosts":postId}});
+    if(user_data.modifiedCount === 0){
+      throw 'Could not save the post successfully';
+    }
+    return true; 
+  };
+  const deletesavedPost = async (userId,postId) => {
+    if(validatiion.validObjectId(userId,"ID"));
+    if(validatiion.validObjectId(postId,"ID"));
+    userId = userId.trim();
+    postId = postId.trim();
+    const userscollection = await users();
+    const user_data = await userscollection.updateOne({_id: ObjectId(userId)},{$pull:{"savedPosts":postId}});
+    if(user_data.modifiedCount === 0){
+      throw 'Could not delete saved post successfully';
+    }
+    return true; 
+  };
+
+
   module.exports = {
     getsavedPost:getsavedPost,
+    addsavedPost:addsavedPost,
+    deletesavedPost:deletesavedPost
   };
