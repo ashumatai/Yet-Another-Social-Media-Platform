c5ae50a233a329991a718b747619a5bf8c09d389
deepali04 <nagwade.deepali@gmail.com>
2022-12-14 17:27:29 -0500

    Added route for image upload using multer

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,22 @@ const postsRoutes= require('./posts');
 const savedPostsRoutes= require('./savedPosts');
 const homeRoutes= require('./home');
 const likesRoutes = require('./likes');
-const followerRoutes = require("./followers")
-const followingRoutes = require("./followings")
+const followerRoutes = require("./followers");
+const followingRoutes = require("./followings");
+const multer = require("multer");
+const path = require('path');
+var fs = require('fs');
+
+var storage = multer.diskStorage({
+  destination : function(req, file, callBack){
+    callBack(null, "./public/uploads");
+  },
+  filename: function(req, file, callBack){
+    callBack( null, file.originalname);
+  },
+});
+var uploadImage = multer({storage:storage}).single("postContent");
+
 const constructorMethod = (app) => {
   app.use('/users', userRoutes);
   app.use('/posts', postsRoutes);
@@ -13,6 +27,18 @@ const constructorMethod = (app) => {
   app.use('/savedPosts', savedPostsRoutes);
   app.use('/home', homeRoutes);
   app.use('/likes', likesRoutes);
+
+  app.post("/uploadImage", (req, res) => {
+    uploadImage(req, res, function (e) {
+      if(e)
+        return res.status(500).json("Failed to upload image");
+      let path = req.file.destination + "/" + req.file.filename;
+      console.log(path);
+      path = path.substring(1);
+      return res.json(path);
+    });
+  });
+
   app.use('*', (req, res) => {
     res.status(404).send("Page not found");
   });
