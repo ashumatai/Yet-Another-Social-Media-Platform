db694add6cd3d8d7aeb587cfe596516a8a615b09
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2022-12-15 00:13:55 -0500

    Minor change

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const postsRoutes= require('./posts');
 const savedPostsRoutes= require('./savedPosts');
 const homeRoutes= require('./home');
 const likesRoutes = require('./likes');
+const filtersRoutes = require('./filters');
 const constructorMethod = (app) => {
   app.use('/users', userRoutes);
   app.use('/posts', postsRoutes);
   app.use('/savedPosts', savedPostsRoutes);
   app.use('/home', homeRoutes);
   app.use('/likes', likesRoutes);
+  app.use('/filters', filtersRoutes);
   app.use('*', (req, res) => {
     res.status(404).send("Page not found");
   });
